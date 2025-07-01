4c3c7ac974a464179344156a646e8a4d21c60246
Soumita Nag <168756525+Soumita-Nag@users.noreply.github.com>
2025-07-02 02:49:35 +0530

    NavBar styles added

--- src/App.vue
+++ src/App.vue
@@ -11,17 +11,28 @@
       IncomeExpenses,
       AddTransaction,
       TransactionHistory,
+    },
+    data(){
+      return{
+        theme:"light",
+      }
     }
   }
 </script>
 
 <template>
-  <div class=" bg-[var(--color-bg)] text-white h-[100vh]">
-    <NavBar/>
-    <Balance/>
-    <IncomeExpenses/>
-    <AddTransaction/>
-    <TransactionHistory/> 
+  <div class=" bg-[var(--color-bg-dark)] text-white h-[100vh] flex flex-col items-center md:p-0 pl-2 pr-2">
+    <div class="xl:w-[60%] lg:w-[70%] md:w-[80%] w-full mt-2">
+      <div>
+        <NavBar :theme="theme"/>
+      </div>
+      <div class="w-full bg-[var(--color-bg-secondary-dark)] mt-2 p-4">
+        <Balance/>
+        <IncomeExpenses/>
+        <AddTransaction/>
+        <TransactionHistory/> 
+      </div>
+    </div>
   </div>
 </template>
 
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,10 @@
 <template>
-    <h2>Expense Tracker</h2>
+    <div class="flex flex-wrap justify-between p-4 bg-[var(--color-bg-secondary-dark)]">
+        <h2>Expense Tracker</h2>
+        <button v-if="theme=='dark'" type="button">☀️</button>
+        <button v-else-if="theme=='light'" type="button">🌙</button>
+
+    </div>
 </template>
 <script>
     export default{
@@ -7,6 +12,9 @@
             return{
                 
             }
+        },
+        props:{
+            theme:String,
         },
         methods:{
             
