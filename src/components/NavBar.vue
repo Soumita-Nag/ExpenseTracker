a12444c338733970f4ffad33891e47d3c946d90e
Soumita Nag <168756525+Soumita-Nag@users.noreply.github.com>
2025-07-03 00:35:27 +0530

    logo name modified

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-wrap justify-between p-4 bg-purple-950/60 rounded-t-xl shadow-md">
     <h2 class="text-white text-[1.6rem] pt-1 font-bold tracking-wide">Expense Tracker</h2>
     <span class="h-[53px] w-[53px] bg-pink-700 flex justify-between items-center rounded-full">
-      <img src="../assets/ET.png" alt="" class="h-[50px] w-[50px] rounded-full border-pink-700 ">
+      <img src="/ET.png" alt="" class="h-[50px] w-[50px] rounded-full border-pink-700 ">
     </span>
   </div>
 </template>
