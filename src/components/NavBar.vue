6d76ecafd4c28c241f1b0119f1b2365a61903aab
Soumita Nag <168756525+Soumita-Nag@users.noreply.github.com>
2025-07-02 18:37:42 +0530

    theme modified: navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="flex flex-wrap justify-between p-4 bg-[var(--color-bg-secondary-dark)]">
-        <h2>Expense Tracker</h2>
-
-    </div>
+  <div class="flex flex-wrap justify-between p-4 bg-gradient-to-r from-violet-700 via-purple-800 to-pink-600 rounded-t-xl shadow-md">
+    <h2 class="text-white text-2xl font-bold tracking-wide">Expense Tracker</h2>
+  </div>
 </template>
+
 <script>
 </script>
+
 <style scoped>
-    
 </style>
