f09ee9e162f8c3f0a50fe3b386d31b8a9fb688c8
Soumita Nag <168756525+Soumita-Nag@users.noreply.github.com>
2025-07-11 21:59:08 +0530

    two separate buttons added for income and expense

--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -7,7 +7,7 @@
       <p class="pl-1 text-sm opacity-80">Add -ve sign in case of expense</p>
     </div>
 
-    <form @submit.prevent="onSubmit" class="space-y-6 w-full">
+    <form @submit.prevent class="space-y-6 w-full">
       <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
         <div class="flex flex-col">
           <label for="text" class="mb-1 text-sm font-medium text-pink-200">Text</label>
@@ -32,13 +32,22 @@
           />
         </div>
       </div>
-
+      <div class="flex gap-5">
       <button
         type="submit"
-        class="w-full cursor-pointer px-6 py-2 bg-pink-600 text-white font-semibold rounded-md hover:bg-pink-700 transition"
+        class="w-1/2 cursor-pointer px-6 py-2 bg-pink-600 text-white font-semibold rounded-md hover:bg-pink-700 transition"
+        @click="addIncome"
       >
-        Add Transaction
+        Add Income
       </button>
+      <button
+        type="submit"
+        class="w-1/2 cursor-pointer px-6 py-2 bg-pink-600 text-white font-semibold rounded-md hover:bg-pink-700 transition"
+        @click="addExpense"
+      >
+        Add Expense
+      </button>
+      </div> 
     </form>
   </div>
 </template>
@@ -53,7 +62,7 @@ const cost = ref('');
 const toast = useToast();
 const emit = defineEmits(['transactionSubmit']);
 
-const onSubmit = () => {
+const addIncome = () => {
   if (!text.value && !cost.value) {
     toast.error("Both fields must be filled!");
     return;
@@ -72,6 +81,30 @@ const onSubmit = () => {
   const transactionData = {
     text: text.value,
     cost: parseFloat(cost.value)
+  };
+  emit('transactionSubmit', transactionData);
+  text.value = '';
+  cost.value = '';
+};
+const addExpense = () => {
+  if (!text.value && !cost.value) {
+    toast.error("Both fields must be filled!");
+    return;
+  } else if (!text.value) {
+    toast.error("Text field cannot be empty!");
+    return;
+  } else if (!cost.value) {
+    toast.error("Amount field cannot be empty!");
+    return;
+  }
+  else if(isNaN(cost.value)){
+    toast.error("Amount must contain Numeric Value!");
+    return;
+  }
+
+  const transactionData = {
+    text: text.value,
+    cost: parseFloat(cost.value*(-1))
   };
   emit('transactionSubmit', transactionData);
   text.value = '';
