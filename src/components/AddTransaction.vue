2f4236ac30d5a1a05bee2fe7dba86e44c82b8025
Soumita Nag <168756525+Soumita-Nag@users.noreply.github.com>
2025-07-11 22:58:53 +0530

    numeric pad for mobile

--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="mx-auto mt-5 p-6 bg-gradient-to-br from-purple-900 via-violet-800 to-pink-700 shadow-md rounded-xl transition-all duration-300 border border-pink-400/30">
+  <div
+    class="mx-auto mt-5 p-6 bg-gradient-to-br from-purple-900 via-violet-800 to-pink-700 shadow-md rounded-xl transition-all duration-300 border border-pink-400/30"
+  >
     <div class="mb-6">
       <h3 class="text-2xl font-semibold text-white pb-2 border-b border-pink-300">
         Add New Transaction
@@ -28,100 +30,98 @@
             placeholder="Enter Amount"
             class="px-4 py-2 rounded-md border border-pink-400/40 bg-purple-950 text-white focus:outline-none focus:ring-2 focus:ring-pink-400"
             v-model="cost"
+            inputmode="numeric"
           />
         </div>
       </div>
       <div class="flex gap-5">
-      <button
-        type="submit"
-        class="w-1/2 cursor-pointer px-6 py-2 bg-pink-600 text-white font-semibold rounded-md hover:bg-pink-700 transition"
-        @click="addIncome"
-      >
-        Add Income
-      </button>
-      <button
-        type="submit"
-        class="w-1/2 cursor-pointer px-6 py-2 bg-pink-600 text-white font-semibold rounded-md hover:bg-pink-700 transition"
-        @click="addExpense"
-      >
-        Add Expense
-      </button>
-      </div> 
+        <button
+          type="submit"
+          class="w-1/2 cursor-pointer px-6 py-2 bg-pink-600 text-white font-semibold rounded-md hover:bg-pink-700 transition"
+          @click="addIncome"
+        >
+          Add Income
+        </button>
+        <button
+          type="submit"
+          class="w-1/2 cursor-pointer px-6 py-2 bg-pink-600 text-white font-semibold rounded-md hover:bg-pink-700 transition"
+          @click="addExpense"
+        >
+          Add Expense
+        </button>
+      </div>
     </form>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
-import { useToast } from "vue-toastification";
+import { ref } from 'vue'
+import { useToast } from 'vue-toastification'
 
-const text = ref('');
-const cost = ref('');
+const text = ref('')
+const cost = ref('')
 
-const toast = useToast();
-const emit = defineEmits(['transactionSubmit']);
+const toast = useToast()
+const emit = defineEmits(['transactionSubmit'])
 
 const addIncome = () => {
   if (!text.value && !cost.value) {
-    toast.error("Both fields must be filled!");
-    return;
+    toast.error('Both fields must be filled!')
+    return
   } else if (!text.value) {
-    toast.error("Text field cannot be empty!");
-    return;
+    toast.error('Text field cannot be empty!')
+    return
   } else if (!cost.value) {
-    toast.error("Amount field cannot be empty!");
-    return;
+    toast.error('Amount field cannot be empty!')
+    return
+  } else if (isNaN(cost.value)) {
+    toast.error('Amount must contain Numeric Value!')
+    return
   }
-  else if(isNaN(cost.value)){
-    toast.error("Amount must contain Numeric Value!");
-    return;
-  }
-  const date=new Date();
-  const day=date.getDate();
-  const month=date.getMonth()+1;
-  const year=date.getFullYear();
-  const today=day+"-"+month+"-"+year;
+  const date = new Date()
+  const day = date.getDate()
+  const month = date.getMonth() + 1
+  const year = date.getFullYear()
+  const today = day + '-' + month + '-' + year
 
   const transactionData = {
     text: text.value,
     cost: parseFloat(cost.value),
-    date: today
-  };
-  emit('transactionSubmit', transactionData);
-  text.value = '';
-  cost.value = '';
-};
+    date: today,
+  }
+  emit('transactionSubmit', transactionData)
+  text.value = ''
+  cost.value = ''
+}
 const addExpense = () => {
   if (!text.value && !cost.value) {
-    toast.error("Both fields must be filled!");
-    return;
+    toast.error('Both fields must be filled!')
+    return
   } else if (!text.value) {
-    toast.error("Text field cannot be empty!");
-    return;
+    toast.error('Text field cannot be empty!')
+    return
   } else if (!cost.value) {
-    toast.error("Amount field cannot be empty!");
-    return;
+    toast.error('Amount field cannot be empty!')
+    return
+  } else if (isNaN(cost.value)) {
+    toast.error('Amount must contain Numeric Value!')
+    return
   }
-  else if(isNaN(cost.value)){
-    toast.error("Amount must contain Numeric Value!");
-    return;
-  }
-  const date=new Date();
-  const day=date.getDate();
-  const month=date.getMonth()+1;
-  const year=date.getFullYear();
-  const today=day+"-"+month+"-"+year;
+  const date = new Date()
+  const day = date.getDate()
+  const month = date.getMonth() + 1
+  const year = date.getFullYear()
+  const today = day + '-' + month + '-' + year
   // alert(today);
   const transactionData = {
     text: text.value,
-    cost: parseFloat(cost.value*(-1)),
+    cost: parseFloat(cost.value * -1),
     date: today,
-  };
-  emit('transactionSubmit', transactionData);
-  text.value = '';
-  cost.value = '';
-};
+  }
+  emit('transactionSubmit', transactionData)
+  text.value = ''
+  cost.value = ''
+}
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
