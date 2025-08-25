40a6a2567f10a5868e04784d1b9fda09514fcd23
Soumita Nag <168756525+Soumita-Nag@users.noreply.github.com>
2025-08-26 00:26:40 +0530

    confirmation before deletion of transaction added

--- src/App.vue
+++ src/App.vue
@@ -1,94 +1,113 @@
 <script setup>
-  import NavBar from './components/NavBar.vue';
-  // import Balance from './components/Balance.vue';
-  import IncomeExpenses from './components/IncomeExpenses.vue';
-  import AddTransaction from './components/AddTransaction.vue';
-  import TransactionHistory from './components/TransactionHistory.vue';
-  import FooterPage from './components/FooterPage.vue'
+import NavBar from './components/NavBar.vue'
+// import Balance from './components/Balance.vue';
+import IncomeExpenses from './components/IncomeExpenses.vue'
+import AddTransaction from './components/AddTransaction.vue'
+import TransactionHistory from './components/TransactionHistory.vue'
+import FooterPage from './components/FooterPage.vue'
 
-  import { ref, computed, onMounted } from 'vue';
-  import { useToast } from "vue-toastification";
+import { ref, computed, onMounted } from 'vue'
+import { useToast } from 'vue-toastification'
+import Swal from 'sweetalert2'
 
-  const toast = useToast();
+const toast = useToast()
 
-  onMounted(() => {
-    const savedTransactions = JSON.parse(localStorage.getItem('transactions'))
-    if (savedTransactions) {
-      transactions.value = savedTransactions;
-    }
-  })
-
-  const updateLocalStorage = () => {
-    localStorage.setItem('transactions', JSON.stringify(transactions.value))
+onMounted(() => {
+  const savedTransactions = JSON.parse(localStorage.getItem('transactions'))
+  if (savedTransactions) {
+    transactions.value = savedTransactions
   }
+})
 
-  const transactions = ref([]);
+const updateLocalStorage = () => {
+  localStorage.setItem('transactions', JSON.stringify(transactions.value))
+}
 
-  const balance = computed(() => {
-    return transactions.value.reduce((acc, t) => acc + parseFloat(t.cost), 0)
+const transactions = ref([])
+
+const balance = computed(() => {
+  return transactions.value.reduce((acc, t) => acc + parseFloat(t.cost), 0)
+})
+
+const income = computed(() => {
+  return transactions.value
+    .filter((transaction) => transaction.cost >= 0)
+    .reduce((acc, t) => acc + parseFloat(t.cost), 0)
+})
+
+const expenses = computed(() => {
+  return transactions.value
+    .filter((transaction) => transaction.cost < 0)
+    .reduce((acc, t) => acc + parseFloat(t.cost), 0)
+})
+
+const saveTransaction = (transactionData) => {
+  transactions.value.unshift({
+    id: generateUniqId(),
+    text: transactionData.text,
+    cost: transactionData.cost.toFixed(2),
+    date: transactionData.date,
+  })
+  toast.success('Transaction added successfully')
+  updateLocalStorage()
+}
+
+const generateUniqId = () => {
+  return Math.floor(Math.random() * 100000)
+}
+
+const deleteTransaction = async (id) => {
+  const result = await Swal.fire({
+    title: 'Are you sure?',
+    text: "This transaction will be deleted permanently!",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#d33',
+    cancelButtonColor: '#3085d6',
+    confirmButtonText: 'Yes, delete it!',
+    cancelButtonText: 'Cancel',
+    background:'#1e0149',
+    color: 'white',
   })
 
-  const income = computed(() => {
-    return transactions.value
-      .filter((transaction) => transaction.cost >= 0)
-      .reduce((acc, t) => acc + parseFloat(t.cost), 0)
-  })
-
-  const expenses = computed(() => {
-    return transactions.value
-      .filter((transaction) => transaction.cost < 0)
-      .reduce((acc, t) => acc + parseFloat(t.cost), 0)
-  })
-
-  const saveTransaction = (transactionData) => {
-    transactions.value.unshift({
-      id: generateUniqId(),
-      text: transactionData.text,
-      cost: transactionData.cost.toFixed(2),
-      date: transactionData.date
-    })
-    toast.success("Transaction added successfully");
-    updateLocalStorage();
-  }
-
-  const generateUniqId = () => {
-    return Math.floor(Math.random() * 100000);
-  }
-
-  const deleteTransaction = (id) => {
-    transactions.value = transactions.value.filter((t) => t.id != id);
+  if (result.isConfirmed) {
+    transactions.value = transactions.value.filter((t) => t.id != id)
     toast.success('Transaction Deleted')
-    updateLocalStorage();
+    updateLocalStorage()
+  } else {
+    toast.info('Deletion Cancelled')
   }
+}
 </script>
 
 <template>
-  <div class="bg-gradient-to-b from-[#080014] to-[#1e0149] text-white flex flex-col items-center md:p-0 pl-2 pr-2 min-h-screen">
+  <div
+    class="bg-gradient-to-b from-[#080014] to-[#1e0149] text-white flex flex-col items-center md:p-0 pl-2 pr-2 min-h-screen"
+  >
     <div class="xl:w-[50%] lg:w-[60%] md:w-[70%] w-full mt-2 mb-11">
       <div>
         <NavBar />
       </div>
-      <div class="w-full bg-purple-950/60 backdrop-blur-md rounded-xl shadow-lg mt-2 p-4 ">
+      <div class="w-full bg-purple-950/60 backdrop-blur-md rounded-xl shadow-lg mt-2 p-4">
         <!-- <Balance  /> -->
         <IncomeExpenses :balance="balance" :income="income" :expenses="expenses" />
         <AddTransaction @transactionSubmit="saveTransaction" />
         <TransactionHistory :transactions="transactions" @deleteTransaction="deleteTransaction" />
       </div>
     </div>
-    <FooterPage/>
+    <FooterPage />
   </div>
 </template>
 
 <style>
-  ::-webkit-scrollbar{
-    width: 8px;
-  }
-  ::-webkit-scrollbar-track{
-    background-color: rgb(59, 0, 87);
-
-  }
-  ::-webkit-scrollbar-thumb{
-    background-color: rgb(255, 0, 174);
-    border-radius: 20px;
-  }
+::-webkit-scrollbar {
+  width: 8px;
+}
+::-webkit-scrollbar-track {
+  background-color: rgb(59, 0, 87);
+}
+::-webkit-scrollbar-thumb {
+  background-color: rgb(255, 0, 174);
+  border-radius: 20px;
+}
 </style>
